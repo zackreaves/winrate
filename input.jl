af95611809dfecc3d038e3fd2e3d12587ae8953e
Zackary <Zackary Reaves>
2022-05-01 12:17:29 -0700

    On branch master  Changes to be committed: 	modified:   input.jl 	description: Removed comments and repaired bugs.

--- input.jl
+++ input.jl
@@ -1,26 +1,26 @@
 function wrinput()
 
-	# Initializing and grabbing data
+	# FIX ME: Needs error checking, everywhere.
+	# FIX ME: Needs to interface with database.
+
+	# Initializing
 	print("Which game? ")
 	game = readline()
 	print("\nHow many players? ")
 	playerNum = parse(Int, readline())
 	print("\n")
-	wlist = Array{Any}(missing, (2, playerNum))
-	#playerInt = parse(Int, playerNum)
-	#rounds = 0
+	wlist = Array{Any}(missing, (playerNum, 2))
 
 
 	# Loop that gets player name and their number of wins, also adds to the total number of rounds based on the number of wins each player has. This loop only works for games that don't have the concept of a tie.
 	for i = 1:playerNum
 		println("Name one of the players: ")
-		wlist[playerNum,1] = readline()
+		wlist[i,1] = readline()
 		println("How many wins did they have? ")
-		wlist[playerNum,2] = parse(Int, readline())
-		#wins = readline()
-		#winNum = parse(Int, wins)
-		#rounds = rounds + winNum
+		wlist[i,2] = parse(Int, readline())
 	end
+
+	rounds = sum(wlist[:,2])
 
 	return game, rounds, wlist
 end
